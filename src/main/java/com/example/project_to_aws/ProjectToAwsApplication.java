8a3c32d9d27eb0e6cab23cd401edd5213d9c1501
Danil Rozsoka <danil.rozsoka@consultant.LEGO.com>
2022-08-01 19:48:09 +0300

    s

---
+++ src/main/java/com/example/project_to_aws/ProjectToAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_to_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_to_aws/ProjectToAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_to_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
